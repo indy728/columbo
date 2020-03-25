81600cabf4e8a3a6893ebaa74597b33c9a2f63f3
Kyle Murray <kdevlinmurray@gmail.com>
2020-03-25 15:11:14 -0700

    gitignore, .env and researchin dependencies

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,43 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 import styled from 'styled-components/native'
+
+const Wrapper = styled.View`
+    /* width: 100%; */
+    display: flex;
+    align-items: center;
+
+`
 
 const HomeText = styled.Text`
     font-size: 30px;
     color: red;
+    margin: 5px 0;
 `
+
+const BlueButton = styled.TouchableOpacity`
+    background-color: blue;
+    width: 80%;
+`
+
+const BlueButtonText = styled.Text`
+    color: white;
+    font-size: 20px;
+    text-align: center;
+`
+
 
 const HomeScreen = () => {
   return (
-        <HomeText>HomeScreen</HomeText>
+        <Wrapper>
+            <HomeText>HomeScreen</HomeText>
+            <BlueButton
+                onPress={() => console.log("you touched the boob")}
+                // title="Touch me"
+                >
+                <BlueButtonText>Touch Me</BlueButtonText>
+            </BlueButton>
+        </Wrapper>
     );
 };
 
